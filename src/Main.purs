c19b5108d8f7531471d5b75e0b5526c163d9ae79
Jan Sipr <sipr.jan@gmail.com>
2017-10-21 18:08:42 +0200

    Some broken code in main loop

--- src/Main.purs
+++ src/Main.purs
@@ -2,12 +2,18 @@ module Main where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
+import Data.Array (head)
+import Data.Foldable (traverse_)
 import Data.Generic (class Generic, gShow)
 import Data.Maybe (Maybe(..), maybe)
-import Data.StrMap (keys)
-import Prelude (class Show, Unit, bind, discard, map, pure, show, unit, ($), (+), (<<<), (<>), (>>=))
-import Screeps.FFI.Game (creeps, getGame)
+import Data.StrMap (StrMap, filter, keys, size, values)
+import Prelude (class Show, Unit(..), bind, discard, map, pure, show, unit, void, when, ($), (+), (<<<), (<>), (==), (>), (>>=))
+import Screeps.FFI.Constants (carry, move, work)
+import Screeps.FFI.Creep (Creep)
+import Screeps.FFI.Game (Game, creeps, getGame, spawns, time)
 import Screeps.FFI.Memory (getMemory, setMemorySegment, getMemorySegment)
+import Screeps.FFI.Structure.Spawn (SpawnOpt, spawnCreep, spawnCreepOpt)
 import Screeps.FFI.Types (Screeps)
 import Screeps.FFI.Utils (nullOrUndefinedToMaybe)
 
@@ -28,10 +34,52 @@ getMem = map nullOrUndefinedToMaybe $ getMemorySegment getMemory "pokus"
 showVal :: Val -> String
 showVal v = "pokus: " <> show v.num
 
-main :: forall e. Eff (screeps :: Screeps, console :: CONSOLE | e) Unit
-main = getMem >>= work
+creepGetMemory :: forall eff val
+    . Creep
+    -> String
+    -> Eff (screeps :: Screeps | eff) val
+creepGetMemory creep field = getMemorySegment creep field
+
+filterHarvesters :: String -> StrMap Creep -> StrMap Creep
+filterHarvesters name creeps' = filter checkHarvester creeps'
   where
-    work v = do
-        let v2 = maybe def modifyVal v
-        log $ showVal v2
-        setMemorySegment getMemory "pokus" v2
+    checkHarvester :: Creep -> Boolean
+    checkHarvester a = unsafePerformEff $ do
+        v <- creepGetMemory a "role"
+        pure $ maybe false ((==) name) $ nullOrUndefinedToMaybe v
+
+type HarvesterMemory = {
+    role :: String
+}
+
+main :: forall e. Eff (screeps :: Screeps, console :: CONSOLE | e) Unit
+main = do
+    g <- getGame
+    when (shouldSpawnHarvester g) $ maybe (pure unit) (\s -> void $ spawnCreepOpt s body ("hv" <> show (time g)) harvOpts) $ head $ values $ spawns g
+    when (shouldSpawnUpgrader g) $ maybe (pure unit) (\s -> void $ spawnCreepOpt s body ("up" <> show (time g)) upgrOpts) $ head $ values $ spawns g
+    traverse_
+  where
+    shouldSpawnHarvester :: Game -> Boolean
+    shouldSpawnHarvester g = 2 > (size $ filterHarvesters "harvester" $ creeps g)
+
+    shouldSpawnUpgrader :: Game -> Boolean
+    shouldSpawnUpgrader g = 2 > (size $ filterHarvesters "harvester" $ creeps g)
+
+    body = [work, carry, move, move]
+
+    harvOpts :: SpawnOpt HarvesterMemory
+    harvOpts =
+        { memory: Just {role: "harvester"}
+        , energyStructures: Nothing
+        , dryRun: Nothing
+        }
+
+    upgrOpts :: SpawnOpt HarvesterMemory
+    upgrOpts =
+        { memory: Just {role: "upgrader"}
+        , energyStructures: Nothing
+        , dryRun: Nothing
+        }
+
+    harvesterActions
+    upgraderActions
